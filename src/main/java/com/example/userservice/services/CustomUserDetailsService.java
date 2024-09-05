7ee21f6523c91916f3f2bbca692d92a26912355e
saichaitanya518 <saichaitanya518@gmail.com>
2024-09-05 18:47:07 +0530

    custom user db is added for security login

---
+++ src/main/java/com/example/userservice/services/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.userservice.services;
+
+import com.example.userservice.models.User;
+import com.example.userservice.repositories.UserRepository;
+import com.example.userservice.security.models.CustomUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    UserRepository userRepository;
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(()->new UsernameNotFoundException("username not found"));
+        System.out.println(user);
+        return new CustomUserDetails(user);
+    }
+}
